68df59aa614336d670acd36c424b553798d5fd63
andxk <kupex@ya.ru>
2023-05-01 13:14:02 +0300

    Add classes Lecturer, Reviewer

--- stud_ment.py
+++ stud_ment.py
@@ -7,17 +7,44 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
  
-    def add_courses(self, course_name):
+    def add_course(self, course_name):
         self.finished_courses.append(course_name)   
  
      
 class Mentor:
+    '''Base class for Lecturer and Reviewer'''
+
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
+    def add_course(self, course_name):
+        self.courses_attached.append(course_name)
+
+
+
+class Lecturer(Mentor):
+    '''Лектор'''
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def rate_lector(self, lector, course, grade):
+        '''Выставление оценки лектору'''
+        if isinstance(lector, Lecturer) and course in self.courses_attached:
+            self.grades.setdefault(course, [])
+            self.grades[course] += [grade]
+
+    
+
+
+class Reviewer(Mentor):
+    '''Проверяющий преподаватель'''
+
     def rate_hw(self, student, course, grade):
+        '''Выставление оценок студентам'''
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
@@ -25,7 +52,16 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
+
+
+
+
+
+
+
+
+
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
